7742bfe69fd10be93c4eec102040837ef127ed9c
oshkines <oshkines@mail.ru>
2025-09-25 21:12:44 +0300

    Lesson 14 25.09.2025

---
+++ listContainer.h
@@ -0,0 +1,178 @@
+#pragma once
+
+#ifndef listContainer_H  
+#define listContainer_H  
+
+template<typename T>
+class listContainer
+{
+private:
+    int Size;
+    template<typename T>
+    class Node {
+    public:
+        Node* pNext;
+        Node* pPrev;
+        T Data;
+        Node(T Data = T(), Node* pNext = nullptr, Node* pPrev = nullptr) {
+            this->Data = Data;
+            this->pNext = pNext;
+            this->pPrev = pPrev;
+        };
+    };
+    Node<T>* Head;
+public:
+    listContainer(/* args */);
+    ~listContainer();
+    void push_back(T value);
+    void Clear();
+    void pop_front();
+    int getSize() { return Size; }
+    void remuveAT(int index);
+    void insert(T value, int index);
+    T& operator [] (const int index);
+    void push_front(T Data);
+};
+
+template<typename T>
+listContainer<T>::listContainer(/* args */)
+{
+    Size = 0;
+    Head = nullptr;
+}
+
+template<typename T>
+listContainer<T>::~listContainer()
+{
+    Clear();
+}
+
+
+template <typename T>
+void listContainer<T>::pop_front()
+{
+
+    Node<T>* Temp = Head;
+    Head = Head->pNext;
+    delete Temp;
+    Size--;                       
+}
+
+template <typename T>
+void listContainer<T>::Clear()
+{
+    while (Size) { 
+        pop_front();
+    }
+}
+
+
+template <typename T>
+void listContainer<T>::push_back(T Data)
+{
+     if (Head == nullptr)
+    {
+        Head = new Node<T>(Data);
+    }
+    else
+    {
+        Node<T>* Current = this->Head; 
+        while (Current->pNext != nullptr)
+        {
+            Current = Current->pNext;
+        }
+        Current->pNext = new Node<T>(Data, nullptr, Current);
+    }
+    Size++;
+}
+
+
+template <typename T>
+T& listContainer<T>::operator[](const int index)
+{
+    int counter = 0; 
+    Node<T>* Current = this->Head; 
+
+    while (Current != nullptr) {
+        if (counter == index)
+        {
+            return Current->Data;
+        }
+
+        Current = Current->pNext;
+        counter++;
+    }
+
+}
+
+template <typename T>
+void listContainer<T>::remuveAT(int index)
+{
+    if (index == 0)
+    {
+        pop_front();
+    }
+    else
+    {
+        Node<T>* previous = this->Head;
+        for (int i = 0; i < index - 1; i++)
+        {
+            previous = previous->pNext; 
+        }
+
+        Node<T>* next = this->Head;
+        for (int i = 0; i < index+1; i++)
+        {
+            next = next->pNext;
+        }
+
+        Node<T>* toDelete = previous->pNext; 
+        previous->pNext = toDelete->pNext;  
+        next->pPrev = toDelete->pPrev;
+        delete toDelete; 
+
+        Size--;
+    }
+}
+
+
+template <typename T>
+void listContainer<T>::push_front(T Data) // добавляет элемент в начало списка
+{
+    Head = new Node<T>(Data, Head); 
+    Size++; 
+}
+
+template <typename T>
+void listContainer<T>::insert(T Data, int index) // добавляет элемент в начало списка
+{
+     if (index == 0)
+    {
+        push_front(Data);
+        return; // интересно так отработает, в 1С без проблем
+    }
+    else
+    {
+        Node<T>* previous = this->Head;  
+        for (int i = 0; i < index - 1; i++)
+        {
+            previous = previous->pNext; 
+        }
+        Node<T>* newNode = new Node<T>(Data, previous->pNext);
+        previous->pNext = newNode;
+        // Данный код можно сократить до одной строчки, будет выглядеть так:
+        //previous -> pNext = new Node<T>(Data, previous -> pNext);
+
+        Node<T>* next = this->Head;
+        for (int i = 0; i < index + 1; i++)
+        {
+            next = next->pNext;
+        }
+        next->pPrev = newNode;
+
+        Size++;
+    }
+}
+
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,98 +1,104 @@
-#include <iostream> 
-#include <vector> 
-#include <list> 
 
-using namespace std;
+	#include <iostream>
+	#include "ConsistentContainer.h"
+	#include "listContainer.h"
 
-template <typename Container>
-void Send_message(Container &container){
-    for (auto element : container)
-    {
-        cout << element;// << ", ";
-        if (element != container.back()){
-          cout <<  ", ";  
-        }
-    }
-    cout << endl << "size: " << size(container) << endl << endl;
-}
+	using namespace std;
 
-template <typename Container>
-void deleteElements(Container& container) {
+	template <typename Container>
+	void Send_message(Container& container) {
+		for (int i = 0; i < container.getSize(); ++i)
+		{
+			cout << container[i];// << ", ";
+			if (i+1 != container.getSize()) {
+				cout << ", ";
+			}
+		}
+		cout << endl << "size: " << container.getSize() << endl << endl;
+	}
 
-    int ArrDelete[] = { 3, 5, 7 };
-    int counter = 0;
+	template <typename Container>
+	void deleteElements(Container& container) {
 
-    for (int j = 0; j < size(ArrDelete); j++)
-    {
-        int i = 0;
-        for (auto it = container.begin(); it != container.end(); ++it)
-        {
-            i++;
-            if (ArrDelete[j] - counter  == i)
-            {
-                container.erase(it);
-                break;
-            }
-        }
-        counter++;
-     }
-}
+		int ArrDelete[] = { 3, 5, 7 };
+		int counter = 0;
 
-template <typename Container>
-void addElements(Container &container){
-    //добавляем элементы в контейнер
-    cout << "Add elements to the collection" << endl;
+		for (int j = 2; j >= 0; j--)
+		{
+			container.remuveAT(ArrDelete[j] - 1);
+		}
+	}
 
-    container.insert(container.begin(), 10);
-    Send_message(container);
+	template <typename Container>
+	void addElements(Container& container) {
 
-    int place = size(container)/2;
+		cout << "Add elements to the collection" << endl;
 
-    int i = 0;
-    for (auto it = container.begin(); it != container.end(); ++it)
-    {
-        if (place == i)
-        {
-            container.insert(it, 20);
-            break;
-        }
-        i++;
-    }
-    Send_message(container);
+		container.insert(10, 0);
+		Send_message(container);
 
-    container.push_back(30);
-    Send_message(container);
-}
+		int place = container.getSize() / 2;
+		container.insert(20, place);
 
-template <typename Container>
-void Starting(Container &container)
-{
-    
-    cout << "We output the contents of the array to the console" << endl;
-    Send_message(container);
+		Send_message(container);
 
-    cout << "Delete Elements from container" << endl;
-    deleteElements(container);
+		container.push_back(30);
+		Send_message(container);
+	}
 
-    Send_message(container);   
-    
-    addElements(container);
+	template <typename Container>
+	void Starting(Container& container)
+	{
 
-}
+		cout << "We output the contents of the array to the console" << endl;
+		Send_message(container);
 
-int main()
-{
+		cout << "Delete Elements from container" << endl;
+		deleteElements(container);
 
-    setlocale(LC_ALL, "Russian"); 
+		Send_message(container);
 
-    cout << "Working with container vector" << endl;
-    vector<int> Vector{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    Starting(Vector);
+		addElements(container);
 
-    cout << "Working with container list" << endl;
-    list<int> List{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	}
 
-    Starting(List);
+	int main()
+	{
 
-    return 0;
-}
+		setlocale(LC_ALL, "Russian");
+
+		cout << "Working with consistent container" << endl;
+		//vector<int> Vector{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+		ConsistentContainer<int> cc;
+		cc.push_back(0);
+		cc.push_back(1);
+		cc.push_back(2);
+		cc.push_back(3);
+		cc.push_back(4);
+		cc.push_back(5);
+		cc.push_back(6);
+		cc.push_back(7);
+		cc.push_back(8);
+		cc.push_back(9);
+		Starting(cc);
+
+		cout << "Working with list container" << endl;
+		//list<int> List{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+		listContainer<int> lc;
+		lc.push_back(0);
+		lc.push_back(1);
+		lc.push_back(2);
+		lc.push_back(3);
+		lc.push_back(4);
+		lc.push_back(5);
+		lc.push_back(6);
+		lc.push_back(7);
+		lc.push_back(8);
+		lc.push_back(9);
+
+
+		Starting(lc);
+
+		return 0;
+	}
